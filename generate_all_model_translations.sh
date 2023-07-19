82757497e6bb85a6f9a052dc774cd2053c81b199
Dave <dst@cs.cmu.edu>
2023-07-19 00:31:48 -0400

    tweak

--- generate_all_model_translations.sh
+++ generate_all_model_translations.sh
@@ -3,5 +3,5 @@ NEPOCHS=20
 EPOCHS=$(for x in `seq $NEPOCHS`; do if [ $x -lt 10 ]; then echo 0$x;  else echo $x; fi;  done)
 
 for i in $EPOCHS; do
-    echo python3 generate_ger_eng_translations.py --model_epoch $i
+    python3 generate_ger_eng_translations.py --model_epoch $i
 done
