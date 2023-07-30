1ffb4e3ad859bdcd6f3ffb1b16a0050c932b4f35
Dave <dst@cs.cmu.edu>
2023-07-30 03:49:11 -0400

    Tweaks

--- model/full_model.py
+++ model/full_model.py
@@ -9,6 +9,12 @@ class TransformerModel(nn.Module):
         '''
         This class assembles the transformer model from the individual submodules created,
         block by block as shown in Figure 1 of the paper.
+           src_vocab_size: number of tokens in encoder's embedding dictionary
+           tgt_vocab_size: number of tokens in decoder's embedding dictionary
+           N: number of encoder/decoder layers
+           d_model: embedding size
+           d_ff: feedfirward layer size
+           h: number of attention heads
         '''
         super(TransformerModel, self).__init__()
 
